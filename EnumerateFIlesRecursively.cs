eab81dc9aab5bb5c35153759d56bcab758e244d1
ShujoyI <shujoyi@gmail.com>
2020-11-13 13:33:39 -0800

    Update EnumerateFIlesRecursively.cs
    Edited function, wrapped it in a way to hopefully test it

--- EnumerateFIlesRecursively.cs
+++ EnumerateFIlesRecursively.cs
@@ -1,18 +1,39 @@
 /* Temporary separate file for notes/testing 
 of EnumerateFilesRecursively function
 */
+using System;
+using System.IO;
+using System.Linq;
 
 // code found on StackOverflow from 2013, will look more into this
-IEnumerable<string> Search(string sDir)
-{
-    foreach (var file in Directory.GetFiles(sDir))
-    {
-        yield return file;                
-    }
+// slightly modified
 
-    foreach (var directory in Directory.GetDirectories(sDir))
-    {
-        foreach(var file in Search(directory))
-            yield return file;
+namespace ConsoleApp {
+
+    class Program {
+        
+        static IEnumerable<string> EnumerateFilesRecursively(string path) {
+
+            // checks every file in folder
+            foreach (var file in Directory.GetFiles(path))
+            {
+                yield return file;                
+            }
+
+            // checks every folder within the directory
+            foreach (var folder in Directory.GetDirectories(path))
+            {
+                // recursively call functions to check number of folders in each subfolder
+                foreach(var file in EnumerateFilesRecursively(folder))
+                    yield return file;
+            }
+        }
+
+        // use this to test (don't know how to run yet though)
+        static void Main(string[] args) {
+
+            var line = EnumerateFilesRecursively(args[0]);
+            Console.WriteLine({f.Line});
+        }
     }
 }
